ea0adc6c26134bc9e8868404b4bc4f4bb7610d23
labbots <admin@labbots.com>
2020-06-09 15:27:36 +0100

    Added misc functions

--- README.md
+++ README.md
@@ -91,6 +91,9 @@ Bash library which provides utility functions and helpers for functional program
   - [json::get_value()](#jsonget_value)
 - [Miscellaneous](#miscellaneous)
   - [misc::check_internet_connection()](#misccheck_internet_connection)
+  - [misc::get_pid()](#miscget_pid)
+  - [misc::get_uid()](#miscget_uid)
+  - [misc::generate_uuid()](#miscgenerate_uuid)
 - [String](#string)
   - [string::trim()](#stringtrim)
   - [string::split()](#stringsplit)
@@ -1909,6 +1912,81 @@ misc::check_internet_connection
 
 - **0**:  If script can connect to internet.
 - **1**: If script cannot access internet.
+
+### misc::get_pid()
+
+Get list of process ids based on process name.
+
+#### Example
+
+```bash
+misc::get_pid "chrome"
+#Ouput
+25951
+26043
+26528
+26561
+```
+
+#### Arguments
+
+- **$1** (Name): of the process to search.
+
+#### Exit codes
+
+- **0**:  If match successful.
+- **2**: Function missing arguments.
+
+#### Output on stdout
+
+- list of process ids.
+
+### misc::get_uid()
+
+Get user id based on username.
+
+#### Example
+
+```bash
+misc::get_uid "labbots"
+#Ouput
+1000
+```
+
+#### Arguments
+
+- **$1** (username): to search.
+
+#### Exit codes
+
+- **0**:  If match successful.
+- **2**: Function missing arguments.
+
+#### Output on stdout
+
+- string uid for the username.
+
+### misc::generate_uuid()
+
+Generate random uuid.
+
+#### Example
+
+```bash
+misc::generate_uuid
+#Ouput
+65bc64d1-d355-4ffc-a9d9-dc4f3954c34c
+```
+
+*Function has no arguments.*
+
+#### Exit codes
+
+- **0**:  If match successful.
+
+#### Output on stdout
+
+- random generated uuid.
 
 ## String
 
--- src/misc.sh
+++ src/misc.sh
@@ -34,3 +34,90 @@ misc::check_internet_connection() {
         return 1
     fi
 }
+
+# @description Get list of process ids based on process name.
+#
+# @example
+#   misc::get_pid "chrome"
+#   #Ouput
+#   25951
+#   26043
+#   26528
+#   26561
+#
+# @arg $1 Name of the process to search.
+#
+# @exitcode 0  If match successful.
+# @exitcode 2 Function missing arguments.
+#
+# @stdout list of process ids.
+misc::get_pid() {
+    [[ $# = 0 ]] && printf "%s: Missing arguments\n" "${FUNCNAME[0]}" && return 2
+
+    ps -ef | grep "${1}" | grep -v grep | awk '{ print $2; }'
+    unset psopts
+}
+
+# @description Get user id based on username.
+#
+# @example
+#   misc::get_uid "labbots"
+#   #Ouput
+#   1000
+#
+# @arg $1 username to search.
+#
+# @exitcode 0  If match successful.
+# @exitcode 2 Function missing arguments.
+#
+# @stdout string uid for the username.
+misc::get_uid() {
+    [[ $# = 0 ]] && printf "%s: Missing arguments\n" "${FUNCNAME[0]}" && return 2
+
+    user_id=$(id ${1} 2> /dev/null)
+    declare -i ret=$?
+    if [[ $ret -ne 0 ]]; then
+        printf "No user found with username: %s" "${1}\n"
+        return 1
+    fi
+
+    printf "${user_id}\n" | sed -e 's/(.*$//' -e 's/^uid=//'
+
+    unset user_id
+}
+
+# @description Generate random uuid.
+#
+# @example
+#   misc::generate_uuid
+#   #Ouput
+#   65bc64d1-d355-4ffc-a9d9-dc4f3954c34c
+#
+# @noargs
+#
+# @exitcode 0  If match successful.
+#
+# @stdout random generated uuid.
+misc::generate_uuid() {
+    # Usage: generate_uuid
+    C="89ab"
+
+    for ((N=0;N<16;++N)); do
+        B="$((RANDOM%256))"
+
+        case "$N" in
+            6)  printf '4%x' "$((B%16))" ;;
+            8)  printf '%c%x' "${C:$RANDOM%${#C}:1}" "$((B%16))" ;;
+
+            3|5|7|9)
+                printf '%02x-' "$B"
+            ;;
+
+            *)
+                printf '%02x' "$B"
+            ;;
+        esac
+    done
+
+    printf '\n'
+}
